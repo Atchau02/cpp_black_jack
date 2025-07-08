3c2e5d84d057b23bb277d60cc219ecef35889d4c
Anh Tu Chau <anhtuchau99@gmail.com>
2025-07-07 21:08:49 -0400

    batman

---
+++ include/blackjack/core/Deck.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include <algorithm>
+#include <random>
+#include <chrono>
+
+#include <iostream>
+
+#include "blackjack/core/Card.h"
+
+namespace blackjack{
+
+    class Deck {
+    private:
+       std::vector<blackjack::Card>  cards_;
+       std::mt19937 rng_;   
+
+        void initializeDeck();
+
+    public:
+        Deck();
+
+        Card draw();
+        size_t size() const;
+
+        void shuffle();
+        void reset();
+        bool isEmpty() const;
+        void printDeck() const;
+    };
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "blackjack/core/Deck.h"
+
+int main(){
+    using namespace blackjack;
+
+    Deck deck;
+
+    Card card = deck.draw();
+
+    std::cout << "Your card is: " << card.toString() << '\n';
+
+    deck.printDeck();
+
+    return 0;
+}
